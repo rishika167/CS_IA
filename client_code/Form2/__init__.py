bc42f7f0b509fd35979198b0ed9b23f502345767
rishikaaak.1692007@gmail.com <rishikaaak.1692007@gmail.com>
2024-12-10 16:52:23 +0000

    Edited settings, Form2 and theme/parameters.yaml

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -1,17 +1,18 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import Form1Template
 from anvil import *
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
-import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.users
 
+class Form1(Form1Template):
 
-class Form2(Form2Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Any code you write here will run when the form opens.
+    anvil.users.login_with_form()
+
+    print('This user has logged in: ' + anvil.users.get_user()['email'])
+
