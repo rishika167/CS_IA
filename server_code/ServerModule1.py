9b3be4b4eeec73b4beac45c1aca900c2e04d1a5c
rishikaaak.1692007@gmail.com <rishikaaak.1692007@gmail.com>
2024-12-10 16:30:06 +0000

    Created Form2; Edited settings, ServerModule1 and Form1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,6 @@
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
 import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
