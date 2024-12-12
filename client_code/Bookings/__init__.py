56ad5a781804610a1853e80e8176a025a0228539
rishikaaak.1692007@gmail.com <rishikaaak.1692007@gmail.com>
2024-12-12 06:11:19 +0000

    Created Bookings and Form1; Edited settings; Deleted Form1

--- client_code/Bookings/__init__.py
+++ client_code/Bookings/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import BookingsTemplate
 from anvil import *
 import anvil.users
 import anvil.server
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Bookings(BookingsTemplate):
     def __init__(self, **properties):
         self.init_components(**properties)
         
