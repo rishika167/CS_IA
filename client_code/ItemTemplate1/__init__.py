146bf4dd83bd9d0adddd4488b51ee4094cf876cb
rishikaaak.1692007@gmail.com <rishikaaak.1692007@gmail.com>
2025-01-09 06:41:36 +0000

    Created ItemTemplate1; Deleted RowTemplate1_copy

--- client_code/ItemTemplate1/__init__.py
+++ client_code/ItemTemplate1/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate1_copyTemplate
+from ._anvil_designer import ItemTemplate1Template
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RowTemplate1_copy(RowTemplate1_copyTemplate):
+class ItemTemplate1(ItemTemplate1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
